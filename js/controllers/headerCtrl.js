df3a39a603fc2cf5acbe72162342b9d4e131de2f
Rafael Penczkoski (Desenvolvimento) <rafael.penczkoski@kmm.local>
2018-01-10 21:17:18 -0200

    web page commit

--- js/controllers/headerCtrl.js
+++
@@ -1,7 +0,0 @@
-angular.module("bitTickets").controller("headerCtrl", function($scope, $location) {
-
-	$scope.isActive = function (viewLocation) { 
-        return viewLocation === $location.path();
-    };
-
-});
